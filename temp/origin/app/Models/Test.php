de5e5b6fc243708c8ccbe3a357baec1ef996fe7b
Ahmad <ahmad.alhourani@xlabtechs.com>
2019-01-30 09:42:36 +0200

    Test

--- temp/origin/app/Models/Test.php
+++ temp/origin/app/Models/Test.php
@@ -40,7 +40,7 @@ class Test extends Model
      * @var array
      */
 
-    protected $sortable = ["id", "name1", "l_name", "email"];
+    protected $sortable = ["id", "name", "full_name", "l_name", "email"];
 
     /**
      * The attributes that are mass assignable.
@@ -48,7 +48,7 @@ class Test extends Model
      * @var array
      */
 
-    protected $fillable = ["name1", "l_name", "email", "sms"];
+    protected $fillable = ["name", "full_name", "l_name", "email", "sms"];
 
     public $timestamps = false;
 
